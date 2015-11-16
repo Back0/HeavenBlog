6a040784669e98c042c345371ee012157632dee2
Heaven <1224644920@qq.com>
2015-11-16 21:01:05 +0800

    service and index have been changed

--- blogserver/router/index.js
+++ blogserver/router/index.js
@@ -7,10 +7,26 @@ module.exports = function(app) {
   		  
   		};
 	app.get('/', function (request, response) {
-		var pathname = url.parse(request.url).pathname,query = url.parse(request.url,true).query,servicecode = query.serviceCode;		
+		var pathname = url.parse(request.url).pathname,
+			query = url.parse(request.url,true).query,
+			servicecode = query.serviceCode;		
 		response.send('nihao')
 	});
 	app.post('', function (request, response,next) {
-		
+		var pathname = url.parse(request.url).pathname,
+			query = url.parse(request.url,true).query,
+			servicecode = query.serviceCode;
+			postData = '';	
+		console.log("-------收到post请求---------")；
+		request.setEncoding("utf8");
+	    //监听data数据传输
+	    request.addListener("data", function(postDataChunk) {
+	      postData += postDataChunk;
+	    });
+        //TODO 这种接受post参数的方式需要改进
+	    request.addListener("end", function() {
+	    	
+
+	    });
 	});
 }
--- blogserver/server/server.js
+++ blogserver/server/server.js
@@ -1,3 +1,9 @@
+var url = require('url'),
+	path = require('paht'),
+	pathconfig = require('../config/pathConfig'),
+	svconfig = require('../config/svConfig'),
+	fileoperate = require('../util/fileOperate'),
+	tool = require('../util/tool');
 module.exports = {
 		
 };
