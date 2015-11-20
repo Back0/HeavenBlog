d31f65426375a4dc9a7f3f4e0d9e2be47c55ab93
Heaven <1224644920@qq.com>
2015-11-20 17:21:58 +0800

    dbconfig

---
+++ blogserver/config/dbConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mysql: {
+    host: '127.0.0.1', 
+    user: 'root',
+    password: 'root',
+    database:'cms', // 前面建的user表位于这个数据库中
+    port: 3306
+  },
+  mongodb:{
+  	host: '127.0.0.1', 
+    database:'HeavenBlog'
+  }
+};
